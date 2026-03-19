e53a3d410e2cc276225bed4a5bd35daea58a8adc
Lazar Hristov <lhristov@gmail.com>
2026-03-19 18:33:11 +0200

    Fix the check eDP service

--- services/check-edp.sh
+++ services/check-edp.sh
@@ -6,8 +6,8 @@ LOG_TAG="check-edp"
 # Wait a bit for DRM to initialize
 sleep 2
 
-# Check eDP status (card0-Unknown-1 is the eDP on Teres-I)
-EDP_STATUS=$(cat /sys/class/drm/card0-Unknown-1/status 2>/dev/null)
+# Check eDP status (card1-eDP-1 is the eDP on Teres-I)
+EDP_STATUS=$(cat /sys/class/drm/card1-eDP-1/status 2>/dev/null)
 
 if [ "$EDP_STATUS" != "connected" ]; then
     echo "[$LOG_TAG] eDP not connected (status: $EDP_STATUS), rebooting in 5 seconds..."
